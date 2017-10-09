b3964e336e00564d07cb6bdbe52cda8bb7e8177b
diana1418 <a20166303@pucp.edu.pe>
2017-10-09 12:02:52 -0500

    Cesar Cipher

---
+++ README.md
@@ -0,0 +1,10 @@
+# CIFRADO CESAR
+En el presente documento podemos encontrar el Diagrama de flujo y el Pseudocódigo para que el código sea más entendible.
+
+* ## Diagrama de Flujo:
+
+
+
+
+
+* ## Pseudocódigo:
---
+++ app.js
@@ -0,0 +1,15 @@
+function cipher(phrase){
+  phrase=prompt('Ingrese una frase');
+  //vamos a utilizar un for para que analize todo el string
+  for(var i=0;i<phrase.length;i++){
+    /*utilizamos un charCodeAt para obtener el indice de
+    donde se encuentra las letras en el ASCII
+     */
+   position=phrase.charCodeAt(i);
+   
+ }
+}
+
+function decipher(displacement){
+
+}
